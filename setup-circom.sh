60d85230408cc8c668e302485bca4bb56f4263d9
Cathie So <socathie@gmail.com>
2023-02-02 00:11:00 +0800

    feat: setup scripts

---
+++ setup-circom.sh
@@ -0,0 +1,10 @@
+curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh
+
+git clone https://github.com/iden3/circom.git
+
+cd circom
+
+cargo build --release
+cargo install --path circom
+
+npm install -g snarkjs@0.4.19
